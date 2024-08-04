4fca5937c0cc221859078a281cef0de0e9617640
eustachekamala <eustachejared25@gmail.com>
2024-08-04 11:58:49 +0300

    I this prohect I used the state with the event to a button, to toogle the text of the to 'ON' when is clicked and to 'OFF' when isn't clicked

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  // Declare a new state variable, which we'll call "count with Destructuring array"
+  const [isOn, setIsOn] = useState(false)// We're setting the initial state here as false, because the button should be "OFF" by default
+  
+  // A function to handle the click event of the button
+  function handleClick() {
+    setIsOn((isOn) => !isOn) // We're setting the state of isOn to the opposite of its current value
+  }
+
+  // Return the JSX for the button
+  return (
+    <button style={{ backgroundColor: isOn ? "#007bff" : "#fff", color: isOn ? "#fff" : "#007bff" }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
